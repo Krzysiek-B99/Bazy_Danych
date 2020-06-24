f8152e88b397bd55642e0fc9587f5c22e68a5401
Krzysiek-B99 <62467906+Krzysiek-B99@users.noreply.github.com>
2020-06-24 23:25:12 +0200

    add zad8

---
+++ zad8.sql
@@ -0,0 +1,30 @@
+CREATE PROCEDURE Fibonacci(@n INT)
+AS 
+
+DECLARE @x INT=0;
+DECLARE @y INT=0;
+DECLARE @z INT=1;
+
+IF @n = 1
+BEGIN
+
+ SET @x=0 ;
+
+END;
+ELSE 
+BEGIN
+	WHILE @n-1 > 1
+	BEGIN
+	 SET @x = @y + @z;
+	 SET @y = @z;
+	 SET @z = @x;
+	 SET @n = @n-1;
+	END;
+END;
+
+RETURN @x;
+
+
+CREATE  TRIGGER DML ON AdventureWorks2017.Persons.Person 
+FOR UPDATE AS
+UPDATE Person SET LastName=upper(LastName)
